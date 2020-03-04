d9ec2e702df1ea191085b6e37b37880593383520
Skyler Burger <skylerburger@gmail.com>
2020-03-04 12:54:22 -0800

    Add certs for https

--- mycoll_project/settings.py
+++ mycoll_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 3rd Party Apps
+    'sslserver',
     'rest_framework',
     'corsheaders',
     # Local Apps
