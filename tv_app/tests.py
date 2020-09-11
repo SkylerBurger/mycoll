aef5b39c235e3d456d9b6a327edfa4a3b4e99005
Skyler Burger <skylerburger@gmail.com>
2020-09-11 10:20:27 -0700

    Coverted setUp method to a create method

--- tv_app/tests.py
+++ tv_app/tests.py
@@ -18,15 +18,14 @@ from .views import (
 
 
 class ShowModelTests(TestCase):
-    def setUp(self):
-        # Dummy user
-        self.user = get_user_model().objects.create_user(
+    def create_show(self):
+        user = get_user_model().objects.create_user(
             username='justatest',
             email='test@test.com',
             password='supersecret',
         )
-        self.show = Show.objects.create(
-            owner=self.user,
+        show = Show.objects.create(
+            owner=user,
             title='Sabrina the Teenage Witch',
             year_first_aired=1996,
             overview='Being a teen is difficult, being a teen witch is impossible.',
@@ -34,9 +33,12 @@ class ShowModelTests(TestCase):
             tmdb_page_link='www.tmdb.org',
         )
 
+        return show, user
+
     def test_show_absolute_url(self):
-        expected = '/api/v1/tv/1'
-        actual = self.show._absolute_url
+        show, user = self.create_show()
+        expected = f'/api/v1/tv/{show.id}'
+        actual = show._absolute_url
         self.assertEqual(expected, actual)
 
     def test_show_list_view(self):
@@ -45,21 +47,56 @@ class ShowModelTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_show_content(self):
-        self.assertEqual(self.show.owner.username, 'justatest')
-        self.assertEqual(self.show.title, 'Sabrina the Teenage Witch')
-        self.assertEqual(self.show.year_first_aired, 1996)
-        self.assertEqual(self.show.overview, 'Being a teen is difficult, being a teen witch is impossible.')
-        self.assertEqual(self.show.image_link, 'www.google.com')
-        self.assertEqual(self.show.tmdb_page_link, 'www.tmdb.org')
+        show, user = self.create_show()
+        self.assertEqual(show.owner.username, 'justatest')
+        self.assertEqual(show.title, 'Sabrina the Teenage Witch')
+        self.assertEqual(show.year_first_aired, 1996)
+        self.assertEqual(show.overview, 'Being a teen is difficult, being a teen witch is impossible.')
+        self.assertEqual(show.image_link, 'www.google.com')
+        self.assertEqual(show.tmdb_page_link, 'www.tmdb.org')
 
     def test_show_str(self):
+        show, user = self.create_show()
         expected = 'Sabrina the Teenage Witch (1996)'
-        actual = str(self.show)
+        actual = str(show)
         self.assertEqual(actual, expected)
 
 class SeasonModelTests(TestCase):
     def setUp(self):
         pass
+
+    def create_season(self):
+        user = get_user_model().objects.create_user(
+            username='justatest',
+            email='test@test.com',
+            password='supersecret',
+        )
+        show = Show.objects.create(
+            owner=user,
+            title='Sabrina the Teenage Witch',
+            year_first_aired=1996,
+            overview='Being a teen is difficult, being a teen witch is impossible.',
+            image_link='www.google.com',
+            tmdb_page_link='www.tmdb.org',
+        )
+        season = Season.objects.create(
+            owner=user,
+            show=show,
+            season_number=1,
+            episode_count=23,
+            year_first_aired=1996,
+            overview='Things are just getting started!',
+            image_link='www.google.com',
+            tmdb_page_link='www.tmdb.org',
+        )
+
+        return season
+
+    def test_season_absolute_url(self):
+        season = self.create_season()
+        expected = '/api/v1/tv/season/1'
+        actual = season._absolute_url
+        self.assertEqual(actual, expected)
 
 
 class SeasonCopyModelTests(TestCase):
