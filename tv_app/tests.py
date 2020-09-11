d9b916ffd9a46ee7abb5ee50b4c9f015fa00e766
Skyler Burger <skylerburger@gmail.com>
2020-09-10 17:58:31 -0700

    Add tests for Show model

--- tv_app/tests.py
+++ tv_app/tests.py
@@ -7,6 +7,14 @@ from .models import (
     Season,
     SeasonCopy,
 )
+from .views import (
+    ShowDetailView,
+    ShowListView,
+    SeasonDetailView,
+    SeasonListView,
+    SeasonCopyDetailView,
+    SeasonCopyListView,
+)
 
 
 class ShowModelTests(TestCase):
@@ -33,8 +41,21 @@ class ShowModelTests(TestCase):
 
     def test_show_list_view(self):
         response = self.client.get(reverse('show_list'))
+        self.assertEqual(response.resolver_match.func.__name__, ShowListView.as_view().__name__)
         self.assertEqual(response.status_code, 200)
 
+    def test_show_content(self):
+        self.assertEqual(self.show.owner.username, 'justatest')
+        self.assertEqual(self.show.title, 'Sabrina the Teenage Witch')
+        self.assertEqual(self.show.year_first_aired, 1996)
+        self.assertEqual(self.show.overview, 'Being a teen is difficult, being a teen witch is impossible.')
+        self.assertEqual(self.show.image_link, 'www.google.com')
+        self.assertEqual(self.show.tmdb_page_link, 'www.tmdb.org')
+
+    def test_show_str(self):
+        expected = 'Sabrina the Teenage Witch (1996)'
+        actual = str(self.show)
+        self.assertEqual(actual, expected)
 
 class SeasonModelTests(TestCase):
     def setUp(self):
